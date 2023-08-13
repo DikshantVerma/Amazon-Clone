20b6132821c56a589a9f01f04ce21f3f24ed5ad4
DikshantVerma <dikshantverma411@gmail.com>
2023-08-13 23:38:24 +0530

    Logo addition plus some css

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
     <nav className="header">
       <img
         className="header__Logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+        src="https://upload.wikimedia.org/wikipedia/commons/3/34/Ecal_logo_print.png"
         alt="Amazon Logo"
       />
     </nav>
