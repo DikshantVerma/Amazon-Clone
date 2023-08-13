565574f54a349d82489cd313715729fe3cd58d8a
DikshantVerma <dikshantverma411@gmail.com>
2023-08-13 23:58:17 +0530

    Installed material UI and icons

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="header">
-      <img
-        className="header__Logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/3/34/Ecal_logo_print.png"
-        alt="Amazon Logo"
-      />
+      <Link to="./">
+        <img
+          className="header__Logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/34/Ecal_logo_print.png"
+          alt="Amazon Logo"
+        />
+      </Link>
+
+      <input type="text" className="header__searchInput" />
     </nav>
   );
 }
