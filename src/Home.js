28c862115bfc7523cf8a3427d355cbe0ec524289
DikshantVerma <dikshantverma411@gmail.com>
2023-08-17 00:11:55 +0530

    Addition of image and gradient

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
-  return <div>Home Page newly created!!!!</div>;
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="
+        https://images-eu.ssl-images-amazon.com/images/G/31/2023/GateWay/August/Unrec/PC1._CB598223457_.jpg"
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default Home;
