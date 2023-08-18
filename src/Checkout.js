d5ce79d295a61f2ceac707f5d0e38a22ef16f886
DikshantVerma <dikshantverma411@gmail.com>
2023-08-18 23:47:33 +0530

    Checkout page subtotal of items

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStateValue } from "./StateProvider";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
@@ -48,7 +49,7 @@ function Checkout() {
       </div>
       {basket.length > 0 && (
         <div className="checkout__right">
-          <h1>Subtotal</h1>
+          <Subtotal />
           {/* <Subtotal /> */}
         </div>
       )}
